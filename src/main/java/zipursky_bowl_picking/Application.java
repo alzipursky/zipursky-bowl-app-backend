9d3a1b0a88bb85a01b8989e3406420cbf1d221af
alzipursky <aaronzipursky@gmail.com>
2016-12-09 13:32:06 -0600

    spring application setup

---
+++ src/main/java/zipursky_bowl_picking/Application.java
@@ -0,0 +1,17 @@
+package zipursky_bowl_picking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by aaronzipursky1 on 12/9/16.
+ */
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+
+}
